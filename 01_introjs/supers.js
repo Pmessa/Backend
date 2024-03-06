9c9235173d5448ff549ddb11ddc77ffa6f24ce4e
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2024-03-06 14:12:01 -0300

    class NoteManager (#3)
    * metodos de classes
    
    * 02_ec
    
    * 02_ec
    
    * new request (#4)
    
    * metodos de classes (#2)
    
    * 01_introjs

--- 01_introjs/supers.js
+++ 01_introjs/supers.js
@@ -15,18 +15,30 @@ super5.edad = 33;
 super5.ciudad = "Asgard";
 // console.log(super5);
 
-
 super4 = { nombre: "Shehulk" };
 // console.log(super4);
 
 function printAll(arrayOfSupers) {
+
     //por cada elemento del array de superheroes 
+
   for (let superheroe of arrayOfSupers) {
     //ejecuta todas estas sentencias
     //each es cada objeto del array de superheroes
 
     // const template = printName(superheroe);
     // console.log(template);
+
+
+    console.log(printName(superheroe));
+  }
+}
+
+// const array1 = [super1 , super2]
+// printAll(array1)
+printAll([super3, super5]);
+
+
     console.log(printName(superheroe))
   }
 }
@@ -34,3 +46,4 @@ function printAll(arrayOfSupers) {
 // const array1 = [super1 , super2]
 // printAll(array1)
 printAll([super3,super5])
+

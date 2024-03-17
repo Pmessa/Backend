03497f605bc211c0a71ce9bd332e1382e6681269
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2024-03-17 18:55:59 -0300

    practica (#6)

---
+++ node-53135/05_prueba_node/server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const router = require("./router.js")
+
+const server = http.createServer(router);
+const port = 8080;
+const ready = () => console.log(`SERVER READY ON PORT ` + port);
+server.listen(port, ready);
